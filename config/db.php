a6fa1acccf337cb1d7c4b1820cebfed33bb130af
David Maranho <53215893+MostWanted17@users.noreply.github.com>
2023-10-24 10:12:33 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost');     // Endereço do servidor do banco de dados
 define('DB_USER', 'root');      // Nome de usuário do banco de dados
 define('DB_PASS', '');      // Senha do banco de dados
-define('DB_NAME', 'transporteschong'); // Nome do banco de dados
+define('DB_NAME', 'transport'); // Nome do banco de dados
 define('DB_PREFIX', '');            // Prefixo para tabelas, se aplicável
 
 // Outras configurações e constantes relacionadas ao banco de dados
