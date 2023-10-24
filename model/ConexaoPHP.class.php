d0219ee93c5bd227bfd35c8e3f5385ebf48fbf5b
David Maranho <53215893+MostWanted17@users.noreply.github.com>
2023-10-24 10:12:13 +0200

    Update ConexaoPHP.class.php

--- model/ConexaoPHP.class.php
+++ model/ConexaoPHP.class.php
@@ -11,7 +11,7 @@ class ConexaoPHP
         $this->host = 'localhost';
         $this->user = 'root';
         $this->senha = '';
-        $this->banco = 'transporte';
+        $this->banco = 'transport';
         $this->prefix = '';
 
         $this->estabelecerConexao();
